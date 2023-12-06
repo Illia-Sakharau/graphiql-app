599d63977d5767ee959f26abd26c4c67392bfb6c
Maxim <fil.maxim13@gmail.com>
2023-12-06 23:40:00 +0300

    feat: add setting test, vitest

---
+++ src/_tests_/test.tsx
@@ -0,0 +1,11 @@
+function Test() {
+  return (
+    <>
+      <div>
+        <h1>It works and you found me!</h1>
+      </div>
+    </>
+  );
+}
+
+export default Test;
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['setupTests.ts'],
+    include: ["src/**/*.{test,spec}.{js,mjs,ts,mts,cts,jsx,tsx}"],
+    globals: true
+  },
 })
