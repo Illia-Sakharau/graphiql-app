519d099eaf07b96d7f6c783f6d72a867dce852bc
Maxim <fil.maxim13@gmail.com>
2023-12-08 12:55:16 +0300

    fix: delete comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
-
 const rootElement = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 root.render(
