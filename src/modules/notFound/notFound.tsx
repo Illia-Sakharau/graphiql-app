6ab1160702750f6e10d1e9098d0fd661bad92944
Maxim <fil.maxim13@gmail.com>
2023-12-13 22:00:42 +0300

    feat: add localization on Not Found Page

--- src/modules/notFound/notFound.tsx
+++ src/modules/notFound/notFound.tsx
@@ -3,9 +3,11 @@ import NotFound from "../../assets/notfound.svg";
 import { NavRoutes } from "../../utils/router/routes";
 import classes from "./notFound.module.scss";
 import Button from "../../UI/button/Button";
+import { useLocalization } from "../../utils/hooks/useLocalization";
 
 export const NotFound404 = () => {
   const navigate = useNavigate();
+  const dictionary = useLocalization();
   const handleBtnClick = () => {
     navigate(NavRoutes.mainPagePath);
   };
@@ -13,7 +15,7 @@ export const NotFound404 = () => {
     <div className={classes.notFound}>
       <div className={classes.photo}>
         <img src={NotFound} alt="Not Found" />
-        <h3>Page Not Found</h3>
+        <h3>{dictionary.not_found_page.title}</h3>
       </div>
 
       <Button
@@ -22,7 +24,7 @@ export const NotFound404 = () => {
         className={classes["home-btn"]}
         onClick={handleBtnClick}
       >
-        Home Page
+        {dictionary.not_found_page.btn_home}
       </Button>
     </div>
   );
