3f8002d13ac422c7692b43821c30560ff2c472c3
Maxim <fil.maxim13@gmail.com>
2023-12-13 21:44:02 +0300

    feat: add not found page2

--- src/modules/notFound/notFound.tsx
+++ src/modules/notFound/notFound.tsx
@@ -4,14 +4,13 @@ import { NavRoutes } from "../../utils/router/routes";
 import classes from "./notFound.module.scss";
 import Button from "../../UI/button/Button";
 
-
 export const NotFound404 = () => {
   const navigate = useNavigate();
   const handleBtnClick = () => {
     navigate(NavRoutes.mainPagePath);
   };
   return (
-    <div className={classes.notFound} >
+    <div className={classes.notFound}>
       <div className={classes.photo}>
         <img src={NotFound} alt="Not Found" />
         <h3>Page Not Found</h3>
@@ -25,7 +24,6 @@ export const NotFound404 = () => {
       >
         Home Page
       </Button>
-    </div >
-  )
-}
-
+    </div>
+  );
+};
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,3 @@
 import { NotFound404 } from "../modules/notFound/notFound";
 
-export const NotFoundPage = () => (
-  <NotFound404 />
-);
+export const NotFoundPage = () => <NotFound404 />;
